311cbc46f63409e31c5fe64d55ae0b3bbd046561
joao igor <joaoigora@gmail.com>
2021-07-24 14:44:26 -0300

    setup finished

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+const app = express()
+
+app.listen(3000, ()=> {
+    console.log("hello world")
+})
