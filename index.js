5201beea687a7e7853d7209201dc3e049c49ac4b
joao igor <joaoigora@gmail.com>
2021-07-24 15:10:41 -0300

    basic inited

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require("express")
-const axios = require("axios")
-const cheerio = require("cheerio")
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const app = express()
+const routes = require("./routes");
 
-app.listen(3000, ()=> {
-    console.log("hello world")
-})
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/", routes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server Runing on port " + PORT);
+});
